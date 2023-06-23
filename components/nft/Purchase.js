4d9cdec84abef067394be88033e03bd8bef8b2bc
kautzar alibani <kautzar.alibani@postpay.io>
2023-06-23 20:41:04 +0700

    handle disconnect wallet in details

--- components/nft/Purchase.js
+++ components/nft/Purchase.js
@@ -4,6 +4,8 @@ import { HiTag } from 'react-icons/hi'
 import { IoMdWallet } from 'react-icons/io'
 import toast, { Toaster } from 'react-hot-toast'
 
+import { ConnectWallet } from '@thirdweb-dev/react'
+
 const style = {
   button: `mr-8 flex items-center py-2 px-12 rounded-lg cursor-pointer`,
   buttonIcon: `text-xl`,
@@ -99,31 +101,39 @@ const Purchase = ({
   return (
     <div className="flex h-20 w-full items-center rounded-lg border border-[#151c22] bg-[#303339] px-12">
       <Toaster position="bottom-left" reverseOrder={false} />
-      {isListed === 'true' ? (
+      {userAddress ? (
         <>
-          <div
-            onClick={() => {
-              enableButton ? buyItem(listing?.id, 1) : null
-            }}
-            className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}
-          >
-            <IoMdWallet className={style.buttonIcon} />
-            <div className={style.buttonText}>Buy Now</div>
-          </div>
-          <div
-            className={`${style.button} border border-[#151c22]  bg-[#363840] hover:bg-[#4c505c]`}
-          >
-            <HiTag className={style.buttonIcon} />
-            <div onClick={makeOffer} className={style.buttonText}>
-              Make Offer
+          {isListed === 'true' ? (
+            <>
+              <div
+                onClick={() => {
+                  enableButton ? buyItem(listing?.id, 1) : null
+                }}
+                className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}
+              >
+                <IoMdWallet className={style.buttonIcon} />
+                <div className={style.buttonText}>Buy Now</div>
+              </div>
+              <div
+                className={`${style.button} border border-[#151c22]  bg-[#363840] hover:bg-[#4c505c]`}
+              >
+                <HiTag className={style.buttonIcon} />
+                <div onClick={makeOffer} className={style.buttonText}>
+                  Make Offer
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}>
+              <IoMdWallet className={style.buttonIcon} />
+              <div className={style.buttonText}>List Item</div>
             </div>
-          </div>
+          )}
         </>
       ) : (
-        <div className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}>
-          <IoMdWallet className={style.buttonIcon} />
-          <div className={style.buttonText}>List Item</div>
-        </div>
+        <>
+          <ConnectWallet />
+        </>
       )}
     </div>
   )
